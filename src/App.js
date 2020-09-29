d7c9c7e2f5c811d9bb0e48fd31261e340d00d13f
kulbhushanbachheti <54018662+kulbhushanbachheti@users.noreply.github.com>
2020-09-29 09:24:34 -0700

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <switch>
+        <Router path ='/' exact />
+      </switch>
+    </Router>
+    </>
+  );
+}
+
+
+export default App;

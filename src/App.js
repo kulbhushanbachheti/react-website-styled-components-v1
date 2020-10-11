b5a68c4a6c52428cc07fda112149949c410b9887
kulbhushanbachheti <54018662+kulbhushanbachheti@users.noreply.github.com>
2020-10-11 10:05:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <switch>
-        <Router path ='/' exact  />
-      </switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+
+                  </Switch>
+      </Router>
     </>
   );
 }
-
 
 export default App;
